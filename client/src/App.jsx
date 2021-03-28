06e470f000d96c735a1e5753a77322818cf3803c
Daniel Ulises <daniel.cau@pm.me>
2021-03-28 17:16:26 +0200

    More testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Welcome from "./Components/Welcome";
 import Chat from "./Components/Chat";
 import "./main.css";
 
 export default function App() {
 	return (
-		<Router>
-			<Switch>
-				<Route exact path="/" component={Welcome} />
-				<Route path="/chat" component={Chat} />
-			</Switch>
+		<Router basename="/chat-app">
+			<Route exact path="/" component={Welcome} />
+			<Route path="/chat" component={Chat} />
 		</Router>
 	);
 }
